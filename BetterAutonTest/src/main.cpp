777d9492bd0085597a3002562df2126d6cc8ea26
VirtualIce <56491625+CodeSample15@users.noreply.github.com>
2020-11-22 15:34:01 -0500

    Update main.cpp

--- BetterAutonTest/src/main.cpp
+++ BetterAutonTest/src/main.cpp
@@ -1,3 +1,4 @@
+  
 /*----------------------------------------------------------------------------*/
 /*                                                                            */
 /*    Module:       main.cpp                                                  */
@@ -10,13 +11,13 @@
 // ---- START VEXCODE CONFIGURED DEVICES ----
 // Robot Configuration:
 // [Name]               [Type]        [Port(s)]
-// inertia              inertial      1               
-// vertencoder          rotation      2               
-// strafeencoder        rotation      3               
-// leftfront            motor         4               
-// leftback             motor         5               
-// rightfront           motor         6               
-// rightback            motor         7               
+// leftfront            motor         13              
+// leftback             motor         8               
+// rightfront           motor         7               
+// rightback            motor         12              
+// inertia              inertial      9               
+// vertencoder          encoder       A, B            
+// strafeencoder        encoder       C, D            
 // ---- END VEXCODE CONFIGURED DEVICES ----
 
 #include "vex.h"
@@ -26,45 +27,95 @@ using namespace vex;
 int xPos = 0;
 int yPos = 0;
 int yawValue = 0;
+int yG = 0;
+int xG = 0;
+
+bool moving = false;
+
+void display() {
+  while(true) {
+    Brain.Screen.setCursor(1, 1);
+    Brain.Screen.print("Vertencoder: %f", vertencoder.position(degrees));
+
+    Brain.Screen.setCursor(2, 1);
+    Brain.Screen.print("Strafeencoder: %f", strafeencoder.position(degrees));
+
+    Brain.Screen.setCursor(3, 1);
+    Brain.Screen.print("Is moving: %f", moving);
+
+    Brain.Screen.setCursor(4, 1);
+    Brain.Screen.print("yG: %d", yG);
+
+    Brain.Screen.setCursor(5, 1);
+    Brain.Screen.print("xG: %d", xG);
+
+    wait(15, msec);
+    Brain.Screen.clearScreen();
+
+  }
+}
+
+float sig(float x) {
+  return (float)1 / 1 + exp(-x);
+}
 
 void moveTo(int x, int y, float speed) {
+  moving = true;
+
   //calculating motor speeds and direction
   double yawModifier = 1; //this is for the inertial sensor not giving the right numbers to be fed into the system (depending on the position of the inertial sensor on the robot)
-
-  double xValue = cos(yawValue / yawModifier) + (x - xPos);
-  double yValue = sin(yawValue / yawModifier) + (y - yPos);
-
-  //applying those values to the motors
-  double frontLeft = (double)(yValue + xValue * speed);
-  double backLeft = (double)(yValue - xValue * speed);
-  double frontRight = (double)(yValue - xValue * speed);
-  double backRight = (double)(yValue + xValue * speed);
 
   vertencoder.setPosition(0, degrees);
   strafeencoder.setPosition(0, degrees);
 
-  leftfront.setVelocity(frontLeft, vex::velocityUnits::pct);
-  leftback.setVelocity(backLeft, vex::velocityUnits::pct);
-  rightfront.setVelocity(frontRight, vex::velocityUnits::pct);
-  rightback.setVelocity(backRight, vex::velocityUnits::pct);
-
   int startPosX = xPos;
   int startPosY = yPos;
 
-  do {
+  while(!(abs(x-xPos + startPosX) < 10 && abs(y-yPos + startPosY) < 10)) {
+    double xValue = cos(yawValue / yawModifier) + (x - xPos);
+    double yValue = sin(yawValue / yawModifier) + (y - yPos);
+
+    //Normalizing the vector
+    float length = sqrt(xValue * xValue + yValue * yValue);
+
+    xValue /= length;
+    yValue /= length;
+    
+    //applying those values to the motors
+    double frontLeft = (double)((yValue + xValue));
+    double backLeft = (double)((yValue - xValue));
+    double frontRight = (double)((yValue - xValue));
+    double backRight = (double)((yValue + xValue));
+
+    leftfront.setVelocity(frontLeft * speed, vex::velocityUnits::pct);
+    leftback.setVelocity(backLeft * speed, vex::velocityUnits::pct);
+    rightfront.setVelocity(frontRight * speed, vex::velocityUnits::pct);
+    rightback.setVelocity(backRight * speed, vex::velocityUnits::pct);
+
+    yG = abs(y-yPos + startPosY);
+    xG = abs(x-xPos + startPosX);
+    
     leftfront.spin(forward);
     leftback.spin(forward);
     rightfront.spin(forward);
     rightback.spin(forward);
 
-    yPos = strafeencoder.position(degrees) + startPosY;
+    yPos = strafeencoder.position(degrees) - startPosY;
     xPos = vertencoder.position(degrees) + startPosX;
-  } while(abs(xPos-x) < 2 && abs(yPos-y) < 2);
+
+  }
+
+  yPos = strafeencoder.position(degrees) - startPosY;
+  xPos = vertencoder.position(degrees) + startPosX;
+
+  strafeencoder.setPosition(0, degrees);
+  vertencoder.setPosition(0, degrees);
 
   leftfront.stop();
   leftback.stop();
   rightfront.stop();
   rightback.stop();
+  moving = false;
 }
 
 void rightinertialturn(double goaldegrees)
@@ -131,5 +182,14 @@ void leftinertialturn(double goaldegrees)
 int main() {
   // Initializing Robot Configuration. DO NOT REMOVE!
   vexcodeInit();
-  
+  thread t(display);
+
+  wait(3, seconds);
+  moveTo(0,700,20);
+  wait(2, seconds);
+  moveTo(-200, 100, 20);
+  wait(1, seconds);
+  //leftinertialturn(90);
+  wait(3, seconds);
+  moveTo(0, 0, 20);
 }
